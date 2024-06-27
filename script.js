e685b60cb5aaacc7351a58eb08ac14ba98d5f14f
Trilok Chaudhary <trilokchy999@gmail.com>
2024-06-27 14:21:58 +0545

    updated js

---
+++ script.js
@@ -0,0 +1,59 @@
+// const person = "Tricch";
+// console.log(person);
+
+// const age = prompt("How old are you?"); //String
+// console.log(Number(age)); //Type Conversion
+
+// const isValid = confirm("Are you above 18?");
+// console.log(isValid);
+
+// const p = prompt("P");
+// const t = prompt("T");
+// const r = prompt("R");
+// let i = (p*t*r)/100;
+// alert(`The interest for you principal is ${i}`);
+
+// const gender = prompt("Enter you gender:");
+// if(gender === 'm'){
+//     alert(`You are male`);
+// }else{
+//     alert(`You are female`);
+// }
+
+// gender === "m"
+//     ? alert(`You are male`)
+//     : gender === "f"
+//         ? alert(`You are female`)
+//         : alert(`Others`);
+
+
+
+// switch(gender){
+//     case "m":
+//         alert("You are male");
+//         break;
+//     case "f":
+//         alert("You are female");
+//         break;
+//     case "o":
+//         alert("You are Other");
+//         break;
+//     default:
+//         alert("Invalid Gender");
+// }
+
+// const grade = prompt("Enter your grade:");
+// switch (true) {
+//     case grade > 80:
+//         alert("A");
+//         break;
+//     case grade > 60 && grade < 80:
+//         alert("B");
+//         break;
+//     default:
+//         alert("C");
+// }
+
+const user = prompt("Enter username:");
+const pass = prompt("Enter password:");
+(user === "trilok" && pass === "trilok") ? alert(`Welcome ${user}`) : alert("Invalild Credentials");
