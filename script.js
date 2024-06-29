e7c73f1a96d547b029df1410dd3d9cb68ad19674
Trilok Chaudhary <trilokchy999@gmail.com>
2024-06-29 12:56:17 +0545

    updated js

--- script.js
+++ script.js
@@ -54,6 +54,22 @@
 //         alert("C");
 // }
 
-const user = prompt("Enter username:");
-const pass = prompt("Enter password:");
-(user === "trilok" && pass === "trilok") ? alert(`Welcome ${user}`) : alert("Invalild Credentials");
+// const user = prompt("Enter username:");
+// const pass = prompt("Enter password:");
+// (user === "trilok" && pass === "trilok") ? alert(`Welcome ${user}`) : alert("Invalild Credentials");
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log("13 * " + i + " = " + 13 * i);
+//     i++;
+// }
+
+// const fname = "trilok";
+// const lname = "chaudhary";
+// const firstName = fname.slice(0, 1).toUpperCase().concat(fname.slice(1, fname.length));
+// console.log(firstName);
+
+// const num1 = prompt("Enter number:");
+// const formattedNumber = num1.toLocaleString();
+// console.log(formattedNumber);
+
