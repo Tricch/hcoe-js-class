57d269de8bda02d8630ee0e4642ae080143e6a81
Trilok Chaudhary <trilokchy999@gmail.com>
2024-06-29 15:29:08 +0545

    updated js

--- script.js
+++ script.js
@@ -73,3 +73,84 @@
 // const formattedNumber = num1.toLocaleString();
 // console.log(formattedNumber);
 
+// const sum = (n1, n2) => {
+//     return n1 + n2;
+// };
+// const result = sum(110, 200)
+// console.log(result);
+
+
+
+// ################################################## Function #############################################################
+
+// 1. Default function
+// const paging = (num = 5) => {
+//     console.log(num);
+// };
+// paging(1); // 1
+// paging(); // 5
+
+
+// 2. Closure function
+
+// 3. IIFE(Immediately Invoked Function Expression)
+// (() => {
+//     console.log("hello");
+// })();
+
+// 4. Explicit function
+// const sum = (n1, n2) => {
+//     return n1 + n2;
+// };
+// const result = sum(110, 200)
+// console.log(result);
+
+// 5. Implicit Function
+// const sum = (n1, n2) => n1 + n2;
+
+// Q1. Write a JS function that reverses a number.
+// const reverse = (num) => Number(String(num).split('').reverse().join(''));
+// let result = reverse(32243);
+// console.log(result);
+
+// Q2. Write a JS program to convert temperature
+// const tempConverter = (temp, deg = 'c') =>
+//     deg === 'c' ? `${temp}F = ${(temp - 32) * 5 / 9}C` : `${temp}C = ${(temp / 5) * 9 + 32}F`;
+// const result = tempConverter(140);
+// console.log(result);
+
+
+// Object
+// const laptop = {
+//     model: "Apple",
+//     name: "M1 Pro",
+//     modelYear: 2021,
+//     age: () => 2024 - laptop.modelYear,
+//     fullName: function () {
+//         return this.model + this.name;
+//     }
+// }
+// const laptopModel = laptop.model;
+// console.log(laptopModel);
+
+// // Destructure
+// const { model, fullName, ...rest } = laptop;
+// console.log(rest);
+
+
+// let prod = {
+//     name: "headphones",
+//     price: 83.7,
+//     discount: "7%"
+// };
+// const getNewProd = (product) => {
+//     const { price, discount, ...rest } = product;
+//     const discountAmount = price > 100 ? price - (price * 0.1) : price - (price * 0.07);
+//     rest.price = discountAmount;
+//     rest.discount = price > 100 ? "10%" : "7%";
+//     return rest;
+// }
+// const newProd = getNewProd(prod);
+// console.log(newProd);
+
+
